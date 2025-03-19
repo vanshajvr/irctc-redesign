480a18a077dce585cd962c538419314b606a6a6d
Vanshaj Verma <140025134+vanshajvr@users.noreply.github.com>
2025-03-20 02:47:37 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+    
+    button.addEventListener("click", () => {
+        alert("Booking system coming soon!");
+    });
+});
